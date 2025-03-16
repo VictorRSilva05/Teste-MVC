5a77bb9f3a94ef841a3acccec9a00e15b39ffdf2
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-16 18:43:47 -0300

    Criado forms menu e cursos

---
+++ Apresentacao/MenuForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Apresentacao
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(127, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Disciplinas";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(127, 174);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Cursos";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(74, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(188, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Navegar até o menu de disciplinas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(83, 156);
+            label2.Name = "label2";
+            label2.Size = new Size(167, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Navegar até o menu de cursos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(101, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Sistema Universidade";
+            // 
+            // MenuForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(306, 246);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "MenuForm";
+            Text = "MenuForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Persistencia/CursoDAL.cs
@@ -0,0 +1,15 @@
+﻿using Modelo;
+
+namespace Persistencia
+{
+    public class CursoDAL
+    {
+        public IList<Curso> TodosOsCursos()
+        {
+            using (var context = new EFContext())
+            {
+                return context.Cursos.ToList();
+            }
+        }
+    }
+}
